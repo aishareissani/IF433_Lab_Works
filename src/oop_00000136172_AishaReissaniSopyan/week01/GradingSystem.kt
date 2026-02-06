44271cf10484a12c7be2cf24141a6d6fab131c21
aishareissani <aishareissani95@gmail.com>
2026-02-06 16:02:02 +0700

    week01: refactor to use val and string templates

--- src/oop_00000136172_AishaReissaniSopyan/week01/GradingSystem.kt
+++ src/oop_00000136172_AishaReissaniSopyan/week01/GradingSystem.kt
@@ -1,8 +1,8 @@
 package oop_00000136172_AishaReissaniSopyan.week01
 
 fun main(){
-    var name: String = "Aisha Reissani"
-    var score: Int = 80
+    val name = "Aisha Reissani"
+    val score = 80
 
-    printIn("Nama: " + name + ", Nilai: " + score)
+    printIn("Nama: $name, Nilai: $score")
 }
