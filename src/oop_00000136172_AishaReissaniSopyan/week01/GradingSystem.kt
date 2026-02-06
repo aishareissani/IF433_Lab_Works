27c876524224b4e2255d249c7a3259578452c0a1
aishareissani <aishareissani95@gmail.com>
2026-02-06 15:58:42 +0700

    week01: initial setup and procedural code

---
+++ src/oop_00000136172_AishaReissaniSopyan/week01/GradingSystem.kt
@@ -0,0 +1,8 @@
+package oop_00000136172_AishaReissaniSopyan.week01
+
+fun main(){
+    var name: String = "Aisha Reissani"
+    var score: Int = 80
+
+    printIn("Nama: " + name + ", Nilai: " + score)
+}
