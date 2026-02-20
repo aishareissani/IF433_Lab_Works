b141f9673aa7dcf406fe6a9c55b55e1e0953755f
aishareissani <aishareissani95@gmail.com>
2026-02-20 15:25:33 +0700

    week03: test all encapsulation logic in main

--- src/oop_00000136172_AishaReissaniSopyan/week03/Employee.kt
+++ src/oop_00000136172_AishaReissaniSopyan/week03/Employee.kt
@@ -21,4 +21,7 @@ class Employee(val name: String){
     fun printStatus(){
         println("Karyawan: $name, Rating: $performaceRating")
     }
+
+    val tax: Double
+        get() = salary * 0.1
 }
