ee2e37183b5d0e977a3cdc720262df0c47b24c32
aishareissani <aishareissani95@gmail.com>
2026-02-20 15:26:28 +0700

    week03: cleanup code

--- src/oop_00000136172_AishaReissaniSopyan/week03/Main.kt
+++ src/oop_00000136172_AishaReissaniSopyan/week03/Main.kt
@@ -3,12 +3,10 @@ package oop_00000136172_AishaReissaniSopyan.week03
 fun main(){
     val e = Employee("Budi")
 
-    e.salary = -1000
     e.salary = 5000000
     println("Gaji: ${e.salary}")
 
     e.increasePerformance()
-//    e.performaceRating = 5
 
     println("Pajak yang harus dibayar: ${e.tax}")
 }
