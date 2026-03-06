d6709c730343b18b087ac95fcef7bef5109b4a1c
aishareissani <aishareissani95@gmail.com>
2026-03-06 15:03:27 +0700

    week05: (task 1) create MathHelper class with overloading

--- src/oop_00000136172_AishaReissaniSopyan/week05/MathHelper.kt
+++ src/oop_00000136172_AishaReissaniSopyan/week05/MathHelper.kt
@@ -1,7 +1,10 @@
 package oop_00000136172_AishaReissaniSopyan.week05
 
-fun hitungLuas(sisi: Int) = sisi * sisi
+class MathHelper{
+    fun hitungLuas(sisi: Int) = sisi * sisi
 
-fun hitungLuas(panjang: Int, lebar: Int) = panjang * lebar
+    fun hitungLuas(panjang: Int, lebar: Int) = panjang * lebar
 
-fun hitungLuas(jariJari: Double) = 3.14 * jariJari * jariJari
+    fun hitungLuas(jariJari: Double) = 3.14 * jariJari * jariJari
+
+}
