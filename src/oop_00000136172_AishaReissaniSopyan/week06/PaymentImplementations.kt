471f462856514be66d3608741da20801cd42507d
aishareissani <aishareissani95@gmail.com>
2026-03-13 15:17:16 +0700

    week06: create Gopay and CreditCard

---
+++ src/oop_00000136172_AishaReissaniSopyan/week06/PaymentImplementations.kt
@@ -0,0 +1,12 @@
+package oop_00000136172_AishaReissaniSopyan.week06
+
+class Gopay : PaymentMethod{
+    override fun pay(amount: Double) {
+        println("Processing Rp$amount via Gopay Server")
+    }
+}
+class CreditCard : PaymentMethod{
+    override fun pay(amount: Double){
+        println("Contacting Bank for Rp$amount")
+    }
+}
