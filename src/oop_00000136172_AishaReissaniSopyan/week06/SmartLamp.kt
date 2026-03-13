31859dad58da480abca8d87184236fa58a166d93
aishareissani <aishareissani95@gmail.com>
2026-03-13 15:54:59 +0700

    week06: (task) create SmartLamp

--- src/oop_00000136172_AishaReissaniSopyan/week06/SmartLamp.kt
+++ src/oop_00000136172_AishaReissaniSopyan/week06/SmartLamp.kt
@@ -2,10 +2,10 @@ package oop_00000136172_AishaReissaniSopyan.week06
 
 class SmartLamp(override val id: String, override val name: String) : SmartDevice, Switchable{
     override fun turnOn(){
-        println("$name turn ON")
+        println("Lamp $name turn ON")
     }
 
     override fun turnOff(){
-        println("$name turn OFF")
+        println("Lamp $name turn OFF")
     }
 }
---
+++ src/oop_00000136172_AishaReissaniSopyan/week06/SmartSpeaker.kt
@@ -0,0 +1,15 @@
+package oop_00000136172_AishaReissaniSopyan.week06
+
+class SmartSpeaker(override val id: String, override val name: String) : SmartDevice, Switchable {
+    override fun turnOn(){
+        println("$name turn ON")
+    }
+
+    override fun turnOff(){
+        println("$name turn OFF")
+    }
+
+    fun playMusic(song: String){
+        println("Memutar lagu \$song dari Spotify")
+    }
+}
