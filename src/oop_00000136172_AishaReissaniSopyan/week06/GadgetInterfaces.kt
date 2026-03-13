1e4d6baafa9f81d257f5322c13f58d0950515c37
aishareissani <aishareissani95@gmail.com>
2026-03-13 15:09:26 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000136172_AishaReissaniSopyan/week06/GadgetInterfaces.kt
@@ -0,0 +1,13 @@
+package oop_00000136172_AishaReissaniSopyan.week06
+
+interface Camera{
+    fun turnOn(){
+        println("Lensa kamera terbuka dan sensor aktif")
+    }
+}
+
+interface Phone{
+    fun turnOn(){
+        println("SInyal seluler mencari jaringan")
+    }
+}
