26278cb7844b91899625374a0896eba9e5605acb
aishareissani <aishareissani95@gmail.com>
2026-03-27 19:40:32 +0700

    week07: (task) create ItemRarity enum

---
+++ src/oop_00000136172_AishaReissaniSopyan/week07/GameModels.kt
@@ -0,0 +1,9 @@
+package oop_00000136172_AishaReissaniSopyan.week07
+
+enum class ItemRarity(val dropChance: Int){
+    COMMON(60),
+    UNCOMMON(20),
+    RARE(15),
+    EPIC(4),
+    LEGENDARY(1)
+}
