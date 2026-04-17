77599a3c88db2787756a585cdf777f2b49d2f92e
aishareissani <aishareissani95@gmail.com>
2026-04-17 21:18:47 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_00000136172_AishaReissaniSopyan/week08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_00000136172_AishaReissaniSopyan.week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId){
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
