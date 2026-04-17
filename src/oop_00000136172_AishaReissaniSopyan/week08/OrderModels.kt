ebe323b2ae69334daa01635f7a4d8f58931f3af2
aishareissani <aishareissani95@gmail.com>
2026-04-17 15:19:32 +0700

    week08: implement chained safe calls and elvis

--- src/oop_00000136172_AishaReissaniSopyan/week08/OrderModels.kt
+++ src/oop_00000136172_AishaReissaniSopyan/week08/OrderModels.kt
@@ -3,4 +3,4 @@ package oop_00000136172_AishaReissaniSopyan.week08
 class City(val name: String)
 class Address(val city: City?)
 class DeliveryDetails(val address: Address?)
-class Order(val deliveryDetails: DeliveryDetails, val totalPrice: Int?)
+class Order(val deliveryDetails: DeliveryDetails?, val totalPrice: Int?)
