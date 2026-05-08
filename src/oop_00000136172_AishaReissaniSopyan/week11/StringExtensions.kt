eaf95edf0d6e16e8796168e7add3e5cf4b2da579
aishareissani <aishareissani95@gmail.com>
2026-05-08 14:49:50 +0700

    week11: create string extension functions

---
+++ src/oop_00000136172_AishaReissaniSopyan/week11/StringExtensions.kt
@@ -0,0 +1,9 @@
+package oop_00000136172_AishaReissaniSopyan.week11
+
+fun String.addGreeting(): String{
+    return "Hello, $this"
+}
+
+fun String.repeatTimes(n: Int): String {
+    return this.repeat(n)
+}
