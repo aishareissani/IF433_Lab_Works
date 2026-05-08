fafbda75e5946845580c2449c3e8055a02161f1b
aishareissani <aishareissani95@gmail.com>
2026-05-08 16:03:31 +0700

    week11: (task) implement device search with let

--- src/oop_00000136172_AishaReissaniSopyan/week11/SmartHomeDashboard.kt
+++ src/oop_00000136172_AishaReissaniSopyan/week11/SmartHomeDashboard.kt
@@ -25,6 +25,13 @@ fun main(){
         device
     }
     homeDevices.add(acUnit)
-
     homeDevices.add(SmartDevice("Picolo's Auto Feeder", "Pet Care", true, 10))
+
+    val searchResult = homeDevices.find { it.category == "Camera" }
+
+    searchResult?.let {
+        println(it.diagnose())
+    }
+
+
 }
